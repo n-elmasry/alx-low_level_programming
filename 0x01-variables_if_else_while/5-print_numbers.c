dc262931b691ac6c541076c3ad56ebf88dfca26b
nada <almassrynada411@gmail.com>
2023-08-11 13:19:11 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*
+*Description: printing numbers of base 10
+* Return: Always 0 (success)
+*/
+int main(void)
+{
+int n;
+
+for (n = 0; n <= 9; n++)
+{
+printf("%d\n", n);
+}
+return (0);
+}
