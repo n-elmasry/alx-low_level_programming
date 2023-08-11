3b355fde6e4ae4c2943882062454c66ab04d5486
nada <almassrynada411@gmail.com>
2023-08-11 13:00:05 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*
+*Description: printing lowercase and upper letters
+*
+* Return: Always 0 (success)
+*/
+int main(void)
+{
+char c;
+
+for (c = 'a'; c >= 'z'; c++)
+{
+while (c != 'q' && c != 'e')
+putchar(c);
+}
+putchar('\n');
+return (0);
+}
