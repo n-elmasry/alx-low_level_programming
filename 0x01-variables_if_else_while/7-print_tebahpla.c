56d2ffa8ce965811698281ebde5996a57a497763
nada <almassrynada411@gmail.com>
2023-08-11 13:48:51 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*
+*Description: alphabet in reverse
+* Return: Always 0 (success)
+*/
+int main(void)
+{
+char a;
+
+for (a = 'z'; a <= 'z'; a--)
+{
+putchar(a);
+}
+putchar('\n');
+return (0);
+}
