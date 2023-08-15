98c194cc728611dd1ef8bbf4ab010e47f720ebc1
nada <almassrynada411@gmail.com>
2023-08-15 14:20:44 -0700

    task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+*_abs - computes value of an integer
+*Description: value of int
+*@i:number to be checked
+*Return: Returns 0
+*/
+
+int _abs(int i)
+{
+if (i >= 0)
+{
+return (i);
+}
+else
+{
+return (-i);
+}
+}
