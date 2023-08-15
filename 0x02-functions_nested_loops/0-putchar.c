3f770b0f59f8f4342e938e995042bb3cc3024db9
nada <almassrynada411@gmail.com>
2023-08-15 08:36:01 -0700

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,24 +6,10 @@
 *Description: printing _putchar
 *
 * Return: Always 0 (success)
-*_putchar - printing char
 */
-int _putchar(char c);
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
+
 int main(void)
 {
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-_putchar('\n');
-
+write(1, "_putchar", 8);
 return (0);
 }
