ef40f0406a08447a02b66a0770634664291b4fa1
nada <almassrynada411@gmail.com>
2023-08-15 10:05:02 -0700

    task 0

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,15 @@
-#include <unistd.h>
 #include "main.h"
+
 /**
-*main - Entry point
+*print_alphabet - print alphabet
 *
 *Description: printing alphabet in lowercase
 * Return: Always 0 (success)
 */
-void print_alphabet(void);
-int main(void)
+
+void print_alphabet(void)
 {
-void print_alphabet(void);
-{
-char c;
+int c;
 
 for (c = 'a'; c <= 'z'; c++)
 {
@@ -19,6 +17,4 @@ _putchar(c);
 }
 _putchar('\n');
 
-}
-return (0);
 }
