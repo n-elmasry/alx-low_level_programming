e6832096cf97faee506b44a9bd5b005eba64cf37
nada <almassrynada411@gmail.com>
2023-08-15 13:37:20 -0700

    task 3

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -4,25 +4,28 @@
 *print_sign - prints the sign of a number
 *
 *Description: prints the sign of a number
-*@n: number to be checked
+*@n:number to be checked
 *Return: Returns 1 if n > 1, 0 if n == 0, -1 if n < 0
 */
 
 int print_sign(int n)
 {
+
 if (n > 0)
 {
-_putchar("+");
+_putchar('+');
 return (1);
 }
+
 else if (n == 0)
 {
-_putchar ("0");
+_putchar ('0');
 return (0);
 }
+
 else
 {
-_putchar ("-");
+_putchar ('-');
 return (-1);
 }
 }
