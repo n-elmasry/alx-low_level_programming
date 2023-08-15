f92990f5b46e90bc9e0efe0d018200a5f4b50470
nada <almassrynada411@gmail.com>
2023-08-15 10:59:12 -0700

    task 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+*_islower - checks for lowercase character
+*
+*Description: checks for lowercase character
+* Return: Always 0 (success)
+*/
+
+int _islower(int c)
+{
+
+int i;
+
+for (i = 'a'; i <= 'z'; i++)
+{
+for (i = 'c')
+{
+return (1);
+}
+return (0);
+}
