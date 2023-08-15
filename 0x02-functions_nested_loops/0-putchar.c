598f7bb7cf5978fe7b2f28a9fdda4b1ed6543f1e
nada <almassrynada411@gmail.com>
2023-08-15 08:19:11 -0700

    00

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,29 @@
+#include <unistd.h>
+
+/**
+*main - Entry point
+*
+*Description: printing _putchar
+*
+* Return: Always 0 (success)
+*_putchar - printing char
+*/
+int _putchar(char c);
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+int main(void)
+{
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+
+return (0);
+}
