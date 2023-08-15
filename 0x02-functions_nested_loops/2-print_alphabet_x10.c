7bc43f4da2b600d0da533417a4900f4fc1529934
nada <almassrynada411@gmail.com>
2023-08-15 10:20:38 -0700

    task2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+*print_alphabet_x10- print alphabet
+*
+*Description: printing alphabet in lowercase
+* Return: Always 0 (success)
+*/
+
+void print_alphabet_x10(void)
+{
+
+int c, i;
+
+for (c = 'a'; c <= 'z'; c++)
+{
+for (i = 0; i < 10; i++)
+putchar(c);
+}
+_putchar ('\n');
+}
