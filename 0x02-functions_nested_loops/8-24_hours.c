d4de2ba4c75bfc394380e21f6bf6bc1ac3c7d45d
nada <almassrynada411@gmail.com>
2023-08-15 16:13:55 -0700

    task 6

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+*jack_bauer - that prints every minute of the day of Jack Bauer
+*Description: that prints every minute of the day of Jack Bauer
+*Return: Returns 0
+*/
+void jack_bauer(void);
+{
+
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+*times_table - prints the 9 times table, starting with 0.
+*Description: prints the 9 times table, starting with 0.
+*Return: Returns 0
+*/
+
+void times_table(void)
+{
+
+
+}
