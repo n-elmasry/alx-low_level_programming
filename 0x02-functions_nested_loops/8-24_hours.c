875d5a55be8e00e2ccafc40d77767a406ac987fb
nada <almassrynada411@gmail.com>
2023-08-16 15:06:37 -0700

    task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -5,7 +5,20 @@
 *Description: that prints every minute of the day of Jack Bauer
 *Return: Returns 0
 */
-void jack_bauer(void);
+void jack_bauer(void)
 {
+int h, m;
 
+for (h = 0; h <= 23; h++)
+{
+for (m = 0; m <= 59; m++)
+{
+_putchar((h / 10) + '0');
+_putchar((h % 10) + '0');
+_putchar(':');
+_putchar((m / 10) + '0');
+_putchar((m % 10) + '0');
+_putchar('\n');
+}
+}
 }
