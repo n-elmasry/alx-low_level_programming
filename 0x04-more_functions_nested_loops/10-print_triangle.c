2f29488ce5205cc0114c159491579773e47d2edd
nada <almassrynada411@gmail.com>
2023-08-17 15:15:57 -0700

    task 5

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+*print_triangle -  prints a triangle
+*@size: s the size of the triangle
+*Description:  prints a triangle
+* Return:Returns 0
+*/
+void print_triangle(int size)
+{
+int i;
+
+}
