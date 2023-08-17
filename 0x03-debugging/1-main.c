54b6a36f74784c1dc80229450cece0ea344714fe
nada <almassrynada411@gmail.com>
2023-08-17 05:12:23 -0700

    0. Multiple mains

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,23 @@
+/**
+* main - causes an infinite loop
+* Return: 0
+*/
+
+int main(void)
+{
+int i;
+
+printf("Infinite loop incoming :(\n");
+
+i = 0;
+
+/*causing the output to go into an infinite loop*/
+while (i < 10)
+{
+putchar(i);
+}
+
+printf("Infinite loop avoided! \\o/\n");
+
+return (0);
+}
