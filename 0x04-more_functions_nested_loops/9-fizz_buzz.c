a9dd125fd0385dd3dc09e268cd549562e3ae81e3
nada <almassrynada411@gmail.com>
2023-08-17 14:29:30 -0700

    task9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+*main -  prints the numbers from 1 to 100
+*Description:  prints a square
+* Return:Returns 0
+*/
+int main(void)
+{
+
+int i;
+
+for (i = 0; i <= 100; i++)
+{
+if (i % 3 == 0)
+{
+printf("Fizz");
+}
+else if (i % 5 == 0)
+{
+printf("Buzz");
+}
+else if (i % 5 == 0 && i % 3 == 0)
+{
+printf("FizzBuzz");
+}
+else
+{
+printf("%d ", i);
+}
+printf("\n");
+}
+return (0);
+}
