450e1d69543b69b1d6650e34588bd59ad5a11831
nada <almassrynada411@gmail.com>
2023-08-17 13:20:52 -0700

    task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -13,8 +13,12 @@ for (i = 0; i < 10; i++)
 {
 for (n = 0; n <= 14; n++)
 {
-_putchar(n + '0');
+if (n > 9)
+{
+_putchar((n / 10) + '0');
 }
+_putchar((n % 10) + '0');
 }
 _putchar('\n');
+}
 }
---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+*print_diagonal - draws a diagonal line
+*@n: is the number of times the character \ should be printed
+*Description: draws a diagonal line
+* Return:Returns 0
+*/
+void print_diagonal(int n)
+{
+
+if (n > 0)
+{
+
+for (i = 0; i < n; i++)
+
+{
+putchar('\');
+}
+_putchar('\n');
+
+}
+
+}
