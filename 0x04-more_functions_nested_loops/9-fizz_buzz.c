a99dfd883cb069092330f22c0f6aa1d075e4b1c5
nada <almassrynada411@gmail.com>
2023-08-17 14:39:13 -0700

    task9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,23 +12,30 @@ int i;
 
 for (i = 0; i <= 100; i++)
 {
-if (i % 3 == 0)
+if ((i % 3) == 0)
 {
 printf("Fizz");
 }
-else if (i % 5 == 0)
+
+else if ((i % 5) == 0)
 {
 printf("Buzz");
 }
-else if (i % 5 == 0 && i % 3 == 0)
+else if ((i % 5) == 0 && (i % 3) == 0)
 {
 printf("FizzBuzz");
 }
 else
 {
-printf("%d ", i);
+printf("%d", i);
 }
+if (i != 100)
+{
+printf(" ");
+}
+}
+
 printf("\n");
-}
+
 return (0);
 }
