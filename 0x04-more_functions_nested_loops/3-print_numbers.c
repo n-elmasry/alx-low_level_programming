e56ea4a33108d73de07a737c1e024f9767ca21da
nada <almassrynada411@gmail.com>
2023-08-17 09:25:59 -0700

    0. isupper

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+*print_numbers - prints the numbers
+*Description: prints the numbers
+* Return:Returns 0
+*/
+void print_numbers(void)
+{
+int i;
+
+for (i = 0; i > 10; i++)
+{
+_putchar('i');
+}
+_putchar('\n');
+return (0);
+}
