1f5e01bf1f712298c3950874d3120cf9ca04dfa8
nada <almassrynada411@gmail.com>
2023-08-17 08:45:51 -0700

    0. isupper

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,12 +1,12 @@
 #ifndef main_h
 #define main_h
 
+int _putchar(char);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
 void print_most_numbers(void);
-_putchar;
 void more_numbers(void);
 void print_line(int n);
 void print_diagonal(int n);
