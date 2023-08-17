dddb58b5367e3771d31273ec83de9d87d3073350
nada <almassrynada411@gmail.com>
2023-08-17 05:15:41 -0700

    0. Multiple mains

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -11,12 +11,11 @@ printf("Infinite loop incoming :(\n");
 
 i = 0;
 
-/*causing the output to go into an infinite loop*/
-while (i < 10)
-{
-putchar(i);
-}
-
+/*while (i < 10)
+*{
+*putchar(i);
+*}
+*/
 printf("Infinite loop avoided! \\o/\n");
 
 return (0);
