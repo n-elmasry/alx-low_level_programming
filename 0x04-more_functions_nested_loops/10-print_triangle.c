a622e7218bf0a80fe05b867f5f295416c44e04b8
nada <almassrynada411@gmail.com>
2023-08-18 07:52:39 -0700

    task 10

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -8,20 +8,27 @@
 */
 void print_triangle(int size)
 {
-int i;
+int i, n, z;
 
 if (size <= 0)
 {
-putchar('\n');
+_putchar('\n');
 }
 else
 {
 for (i = 0; i < size; i++)
 {
+for (n = size - i; n > 1; n--)
+{
+_putchar(32);
+}
 
+for (z = 0; z <= i; z++)
+{
 _putchar(35);
 }
 
-}
 _putchar('\n');
+}
+}
 }
