97e8f90327b8229de2f38a431c60f1ae781f545e
nada <almassrynada411@gmail.com>
2023-08-21 11:43:09 -0700

    arrays

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+*_strlen - returns the length of a string
+*@s: lenght
+*Description: returns the length of a string
+* Return: s
+*/
+int _strlen(char *s)
+{
+int c = 0;
+
+for (; *s != '\0'; *s++)
+{
+c++;
+}
+}
