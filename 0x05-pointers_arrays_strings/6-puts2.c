956c8f206a0c63a36ee9c53a533f92e7583896cf
nada <almassrynada411@gmail.com>
2023-08-21 15:39:59 -0700

    arrays

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+*puts2 - prints every other character of a string
+*@str:string to be printed
+*Description:  prints every other character of a string
+* Return: str
+*/
+
+void puts2(char *str)
+{
+int i;
+
+for (i = 0; str[i] != '\0'; i++)
+{
+if (i % 2 == 0)
+{
+_putchar(str[i]);
+}
+}
+_putchar('\n');
+
+}
