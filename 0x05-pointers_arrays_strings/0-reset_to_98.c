8a401b444a7018183bcc5f1d4ac5b1f00b237e1c
nada <almassrynada411@gmail.com>
2023-08-21 11:01:53 -0700

    arrays

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+
+/*
+*reset_to_98 - updates the value it points to to 98
+*@*n: retrun int
+*Description: updates the value it points to to 98
+* Return: int
+*/
+
+void reset_to_98(int *n)
+{
+int *p;
+
+p = n;
+*p == 98;
+
+}
