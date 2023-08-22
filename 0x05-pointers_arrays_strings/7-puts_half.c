59a3571a7639e2997f3380fd7bf4e4634226088d
nada <almassrynada411@gmail.com>
2023-08-22 14:28:25 -0700

    task 9

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+
+/**
+*puts_half - prints half of a string
+*@str: to be printed
+*Description: prints half of a string
+* Return:  0
+*/
+
+void puts_half(char *str)
+{
+
+_putchar('\n');
+}
