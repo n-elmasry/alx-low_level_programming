8a4ddffae8b9497b0cf8d2989283bdc1842017f7
nada <almassrynada411@gmail.com>
2023-08-24 10:53:12 -0700

    task3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+*_strcmp -  compares two strings.
+*@s1: first string
+*@s2: second string
+*Description: compares two strings.
+* Return: 0
+*/
+
+int _strcmp(char *s1, char *s2)
+{
+int i = 0, j = 0;
+
+
+if (s2[j] < s1[i])
+{
+return (15);
+}
+else if (s1[i] < s2[j])
+{
+return (-15);
+}
+else
+{
+return (0);
+}
+}
