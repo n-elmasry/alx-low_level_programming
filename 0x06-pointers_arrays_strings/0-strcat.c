53d155049e09edf154a67faadcd1003d00b345f9
nada <almassrynada411@gmail.com>
2023-08-24 08:17:24 -0700

    task0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+*_strcat - concatenates two strings
+*@dest: destination string
+*@src: source string
+*Description: concatenates two strings
+* Return: dest
+*/
+char *_strcat(char *dest, char *src)
+{
+
+int i, j;
+
+while (dest[i] != '\0')
+{
+_putchar(i);
+i++;
+}
+_putchar('\0');
+
+while (src[j]++)
+{
+_putchar (j);
+}
+
+return (dest);
+}
