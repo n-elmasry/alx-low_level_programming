c14b6d112258c14e7454f7ec2916ef18e806bacf
nada <almassrynada411@gmail.com>
2023-08-24 13:49:06 -0700

    task3

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+*string_toupper - changes all lowercase letters of a string to uppercase
+*Description: changes all lowercase letters of a string to uppercase
+* Return: 0
+*/
+
+char *string_toupper(char *s)
+{
+
+int i = 0;
+
+while(s[i])
+{
+
+if (s[i] >= 'a' && s[i] <= 'z')
+{
+s[i] = s[i] - 32;
+}
+i++;
+
+}
+return (s);
+}
