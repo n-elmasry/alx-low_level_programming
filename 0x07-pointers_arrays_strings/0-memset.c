934a1b97a84903b2ada44683dfd4d4fa63f17e05
nada <almassrynada411@gmail.com>
2023-08-28 08:34:18 -0700

    task 0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+*_memset -  fills memory with a constant byte
+*@s: a pointer to the block of memory to fill
+*@b: the value to be set.
+*@n:his is the number of bytes to be set to the value.
+*Description:  fills memory with a constant byte
+* Return: s
+*/
+
+char *_memset(char *s, char b, unsigned int n)
+{
+
+unsigned int i;
+
+while (n)
+{
+for (i = 0; i <= n; i++)
+{
+s[n] = b;
+}
+}
+return (s);
+}
