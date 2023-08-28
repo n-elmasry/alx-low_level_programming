7e6fc1c13227d28b700ba1f705f0d98d6a92fd3f
nada <almassrynada411@gmail.com>
2023-08-28 08:46:27 -0700

    task 0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -14,12 +14,13 @@ char *_memset(char *s, char b, unsigned int n)
 
 unsigned int i;
 
-while (n)
+while (s[n])
 {
-for (i = 0; i <= n; i++)
+for (i = 0; i < n; i++)
 {
 s[n] = b;
 }
 }
-return (s);
+return(s);
 }
+
