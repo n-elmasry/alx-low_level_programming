522078fc01018c1c4b873bac7f8d043c2d6f11ef
nada <almassrynada411@gmail.com>
2023-08-28 10:22:43 -0700

    task 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stddef.h>
+/**
+*_strchr - locates a character in a string
+*@s: string to be scanned
+*@c: character to be searched in s
+*Description: locates a character in a string
+* Return: pointer to character c in s, or NULL if the character is not found.
+*/
+char *_strchr(char *s, char c)
+{
+
+int i;
+
+for (i = 0; s[i] != '\0'; i++)
+{
+if (s[i] == c)
+{
+return (&(s[i]));
+}
+}
+return (NULL);
+}
