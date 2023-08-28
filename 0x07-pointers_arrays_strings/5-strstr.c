4dc4a5ce1d0004a90bed0c4b5fdbefbf3c37d9ba
nada <almassrynada411@gmail.com>
2023-08-28 14:56:38 -0700

    task5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -5,7 +5,7 @@
 *_strstr -   locates a substring
 *@haystack: string to be scanned
 *@needle:  string to be searched with-in haystack string
-* Return: pointer to the beginning of the located substring, or NULL if the substring is not found
+* Return: pointer tothe located substring,NULL if the substring is not found
 */
 char *_strstr(char *haystack, char *needle)
 {
