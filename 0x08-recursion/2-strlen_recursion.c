de8f963a63af1bd3027408c552b74df412510d9f
nada <almassrynada411@gmail.com>
2023-08-30 09:20:56 -0700

    task 1

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -2,18 +2,18 @@
 
 /**
 *_strlen_recursion -  returns the length of a string
-*@s: string to be measured 
+*@s: char of a string
 * Return: length of s
 */
 int _strlen_recursion(char *s)
 {
 
-if(*s == '\0')
+if (*s == '\0')
 {
-return(0);
+return (0);
 }
 else
 {
-return 1 + _strlen_recursion(s + 1);
+return (1 + _strlen_recursion(s + 1));
 }
 }
