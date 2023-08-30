52405caac9c8bf36d4d72a578d70387b389dc624
nada <almassrynada411@gmail.com>
2023-08-30 07:36:33 -0700

    task 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -2,21 +2,20 @@
 
 /**
 *_puts_recursion - prints a string, followed by a new line.
-*@s: char to be printed 
-* Return: Always 0 (success)
+*@s: char to be printed
+* Return: 0
 */
 void _puts_recursion(char *s)
 {
 
 if (*s == '\0')
-		
 {
 _putchar('\n');
-return ;
+return;
 }
 else
 {
-_putchar (*s);
-_puts_recursion (s + 1);
+_putchar(*s);
+_puts_recursion(s + 1);
 }
 }
