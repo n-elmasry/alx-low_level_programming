f2da31420bbeab48413b40911aa24c73d568a677
nada <almassrynada411@gmail.com>
2023-08-30 08:26:46 -0700

    task 1

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+*_print_rev_recursion - prints a string in reverse
+*@s: char to be printed
+* Return: Always 0 (success)
+*/
+
+void _print_rev_recursion(char *s)
+{
+if (*s == '\0')
+{
+s--;
+}
+else
+{
+_putchar(*s);
+_print_rev_recursion(s - 1);
+}
+}
