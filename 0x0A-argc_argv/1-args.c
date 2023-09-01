f98c8540a94bd602c5dd37ff72772409535916d4
nada <almassrynada411@gmail.com>
2023-09-01 12:19:44 -0700

    task1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -3,11 +3,13 @@
 #define UNUSED(x) (void)(x)
 /**
 *main - prints the number of arguments passed into it
-* Return: 0
+*@argc:  the number of arguments on the command line.
+*@argv: array of character pointers listing all the arguments
+*Return: 0
 */
-int main (int argc, char* argv[])
+int main(int argc, char *argv[])
 {
 UNUSED(argv);
 printf("%d\n", argc);
-return(0);
+return (0);
 }
