187aab8e35f486a022f5101481a6c521c67796fb
nada <almassrynada411@gmail.com>
2023-09-01 12:02:05 -0700

    task1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+include "main.h"
+/**
+*main - prints the number of arguments passed into it
+* Return: 0
+*/
+int main (int argc, char* argv[])
+{
+printf("%d \n", argc);
+return(0);
+}
