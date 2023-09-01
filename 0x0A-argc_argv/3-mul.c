dc3c8a145a106cd18f24a9110e497b7acd4f9a79
nada <almassrynada411@gmail.com>
2023-09-01 13:48:13 -0700

    task3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,18 +1,20 @@
 #include <stdio.h>
-#include "main.h"
 #include <stdlib.h>
 /**
-*main -  prints all arguments it receives.
+*main - multiplies two numbers.
 *@argc:  the number of arguments on the command line.
 *@argv: array of character pointers listing all the arguments
 *Return: 0
 */
 int main(int argc, char *argv[])
 {
-int i;
-for (i = 0; i < argc; i++)
+	int x = atoi(argv[1]);
+	int y = atoi(argv[2]);
+if (argc != 3)
 {
-printf("%s\n", argv[i]);
+printf("Error\n");
+return (1);
 }
+printf("%d\n", y * x);
 return (0);
 }
