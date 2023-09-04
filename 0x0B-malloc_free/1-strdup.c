84f8e3af766198aa8d3ae620a11c849c9832f473
nada <almassrynada411@gmail.com>
2023-09-04 12:32:24 -0700

    task1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+*_strdup -  returns a pointer to a newly allocated space in memory
+*@str: chars in array
+* Return: Returns a pointer to the array, or NULL if it fails
+*/
+char *_strdup(char *str)
+{
+unsigned int i;
+char *ptr;
+
+if (str == NULL)
+{
+return (NULL);
+}
+
+ptr = malloc(sizeof(str));
+
+if (ptr == NULL)
+{
+return (NULL);
+}
+
+for (i = 0 ; i < sizeof(str); i++)
+{
+ptr[i] = str[i];
+}
+
+return (ptr);
+}
