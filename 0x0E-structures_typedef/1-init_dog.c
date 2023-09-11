2f52cf34773964d54ce770b4f699581d9ff8b2a4
nada <almassrynada411@gmail.com>
2023-09-11 10:14:56 -0700

    task 1

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "dog.h"
+/**
+*init_dog- function
+*@d:struct
+*@name: name of the dog
+*@age: age of the dog
+*@owner: name of owner
+* Return: 0
+*/
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+d->name = name;
+d->age = age;
+d->owner = owner;
+
+}
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef dog_h
+#define dog_h
+/**
+ * struct dog - Short description
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner:name of the owner
+ *
+ * Description: Longer description
+ */
+
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+
+#endif
