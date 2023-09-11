4eb5912c19953f97387c92b763baa6ee8e1cee7a
nada <almassrynada411@gmail.com>
2023-09-11 13:30:48 -0700

    task3

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,21 +1,6 @@
 #ifndef dog_h
 #define dog_h
 /**
- * struct dog - Short description
- * @name: name of the dog
- * @age: age of the dog
- * @owner:name of the owner
- *
- * Description: Longer description
- */
-
-struct dog
-{
-char *name;
-float age;
-char *owner;
-};
-/**
 *struct dog - Short description
 *@name: name of the dog
 *@age: age of the dog
@@ -38,6 +23,4 @@ char *owner;
 */
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
 #endif
