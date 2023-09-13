06d0e2a01fc3c65d8318ef236f1cdbf03bcce2ec
nada <almassrynada411@gmail.com>
2023-09-13 09:29:35 -0700

    task 0 and 1

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -3,11 +3,14 @@
 /**
 *print_name -  prints a name.
 *@name: name to be printed
-*@f: function to print name
+*@char: char to be printed
 * Return: void
 */
 
 void print_name(char *name, void (*f)(char *))
 {
+if (name && f)
+{
 f(name);
+}
 }
