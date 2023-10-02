30fa310519151cfd294a524a0bcd4ae27f1c1671
nada_elmasry <almassrynada411@gmail.com>
2023-10-02 13:54:53 -0700

    task2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -9,13 +9,14 @@
 int append_text_to_file(const char *filename, char *text_content)
 {
 int file;
-ssize_t written, len = 0;
+ssize_t written;
+size_t len = 0;
 
 if (filename == NULL)
 return (-1);
 
 if (text_content == NULL)
-return (-1);
+return (1);
 
 file = open(filename, O_WRONLY | O_APPEND);
 if (file < 0)
@@ -23,7 +24,7 @@ return (-1);
 
 len = strlen(text_content);
 written = write(file, text_content, len);
-if (written == (ssize_t)-1)
+if (written == -1)
 {
 close(file);
 return (-1);
