dd4de147533497684ea264b7c17c9e08d62be224
nada_elmasry <almassrynada411@gmail.com>
2023-10-02 12:33:32 -0700

    task1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <string.h>
+/**
+*create_file -  creates a file.
+*@filename:  the name of the file to create
+*@text_content: a NULL terminated string to write to the file
+*Return: 1 on success, -1 on failure
+*/
+
+int create_file(const char *filename, char *text_content)
+{
+int file, written;
+size_t len = 0;
+
+if (filename == NULL)
+return (-1);
+
+file = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+if (file < 0)
+return (0);
+
+if (text_content != NULL)
+{
+len = strlen(text_content);
+written = write(file, text_content, len);
+
+if (written < 0)
+{
+close(file);
+return (-1);
+}
+}
+close(file);
+return (1);
+}
