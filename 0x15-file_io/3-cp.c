cbd1a4279ba26d8908f15e95265555557eb79a7b
nada_elmasry <almassrynada411@gmail.com>
2023-10-02 17:03:52 -0700

    task3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -8,7 +8,7 @@
 
 int main(int argc, char *argv[])
 {
-FILE *file_from; 
+FILE *file_from;
 int file_to;
 char buffer[1024];
 ssize_t reads, written;
@@ -20,14 +20,13 @@ exit(97);
 }
 
 file_from = fopen(argv[1], "r");
-if(file_from == NULL)
+if (file_from == NULL)
 {
 dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 exit(98);
 }
-file_to = open(argv [2], O_WRONLY | O_CREAT | O_TRUNC | O_APPEND | S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
-
-if(file_to < 0)
+file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+if (file_to < 0)
 {
 dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 exit(99);
@@ -35,7 +34,7 @@ exit(99);
 while ((reads = fread(buffer, 1, sizeof(buffer), file_from)) > 0)
 {
 written = write(file_to, buffer, reads);
-if(written < 0)
+if (written < 0)
 {
 perror("Error writing to file_to");
 exit(99);
@@ -43,5 +42,11 @@ exit(99);
 }
 fclose(file_from);
 close(file_to);
+
+if (close(file_to) != 0)
+{
+dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to);
+exit(100);
+}
 return (0);
 }
