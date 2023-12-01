0b575c9867e16728a030ae91b64569f8880d2581
n-elmasry <almassrynada411@gmail.com>
2023-12-01 15:44:13 -0800

    adv

---
+++ 0x1A-hash_tables/100-sorted_hash_table.c
@@ -0,0 +1,66 @@
+#include "hash_tables.h"
+/**
+* shash_table_create - creates a hash table.
+* @size:  the size of the array
+* Return: a pointer to the newly created hash table or NULL
+*/
+shash_table_t *shash_table_create(unsigned long int size)
+{
+}
+
+/**
+ * shash_table_set - function
+ * @ht: ht
+ * @key: key
+ * @value: value
+ * Return: hash value
+ */
+int shash_table_set(shash_table_t *ht, const char *key, const char *value)
+{
+}
+
+/**
+ * shash_table_get - ifunction
+ * @ht: ht
+ * @key: key
+ * Return: hash value
+*/
+
+char *shash_table_get(const shash_table_t *ht, const char *key)
+{
+
+}
+
+/**
+ * shash_table_print - function
+ * @ht: ht
+ *
+ * Return: hash value
+*/
+void shash_table_print(const shash_table_t *ht)
+{
+
+}
+
+
+/**
+ * shash_table_print_rev - function
+ * @ht: ht
+ *
+ * Return: hash value
+*/
+void shash_table_print_rev(const shash_table_t *ht)
+{
+
+}
+
+/**
+ * shash_table_delete - function
+ * @ht: ht
+ *
+ * Return: hash value
+*/
+void shash_table_delete(shash_table_t *ht)
+{
+
+}
