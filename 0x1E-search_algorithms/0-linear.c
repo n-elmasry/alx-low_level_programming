faea752f17381081f156fda699ffbd56612f03a6
n-elmasry <almassrynada411@gmail.com>
2024-05-07 22:34:16 +0500

    task 0 update

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -13,20 +13,16 @@ int linear_search(int *array, size_t size, int value)
 	size_t i;
 
 	if (array == NULL)
-	{
 		return (-1);
+
+
+	for (i = 0; i < size; i++)
+	{
+		printf("checked array[%ld] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+			return (i);
+
 	}
-
-		for (i = 0; i < size; i++)
-		{
-			printf("checked array[%lu] = [%d]\n", i, array[i]);
-
-			if (array[i] == value)
-			{
-				return (i);
-			}
-
-		}
 
 	return (-1);
 }
